3bf02e7fb8c3bdab4cb1a80ebda40d8b1e96659c
Raj Kumar Gubbala <rijkumar@sjmedialabs.com>
2025-12-18 10:50:37 +0530

    Hi

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\Satya Bhaskar\Desktop\scout\app\layout.tsx
+// File: C:\Users\RAJ KUMAR GUBBALA\Desktop\Spark\scout\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
